76f05131589424d17dd765ac57eb062306fa0e21
Brett Earle <brettearle@gmail.com>
2022-12-04 22:07:56 +1030

    add telescope for git and string

--- lua/tee/telescope.lua
+++ lua/tee/telescope.lua
@@ -3,6 +3,8 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fd', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 
 vim.keymap.set('n', '<leader>z', '<cmd>Telescope file_browser<CR>', {})
 
